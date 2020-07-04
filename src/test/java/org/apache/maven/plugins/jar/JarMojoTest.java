4548405b80fd9ed626e6fb15f57432058491f262
Elliotte Rusty Harold <elharo@ibiblio.org>
2020-07-04 09:09:11 -0400

    simplify test

--- src/test/java/org/apache/maven/plugins/jar/JarMojoTest.java
+++ src/test/java/org/apache/maven/plugins/jar/JarMojoTest.java
@@ -34,26 +34,14 @@ public class JarMojoTest
 {
     private File testPom = new File( getBasedir(), "src/test/resources/unit/jar-basic-test/pom.xml" );
 
-    protected void setUp()
-        throws Exception
-    {
-
-        // required for mojo lookups to work
-        super.setUp();
-
-    }
-
     /**
-     * tests the proper discovery and configuration of the mojo
+     * Tests the discovery and configuration of the mojo.
      *
-     * @throws Exception in case of an error.
+     * @throws Exception in case of an error
      */
     public void testJarTestEnvironment()
         throws Exception
     {
-
-        // File pom = new File( getBasedir(), "src/test/resources/unit/clean/pom.xml" );
-
         JarMojo mojo = (JarMojo) lookupMojo( "jar", testPom );
 
         assertNotNull( mojo );
