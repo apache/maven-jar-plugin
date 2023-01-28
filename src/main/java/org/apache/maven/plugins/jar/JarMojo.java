4b888dd8b2642f33404bcea96f203b33c7929c9c
Jorge Solórzano <jorsol@gmail.com>
2023-01-28 19:56:05 +0100

    [MJAR-293] Update Parent to 39
    Signed-off-by: Jorge Solórzano <jorsol@gmail.com>

--- src/main/java/org/apache/maven/plugins/jar/JarMojo.java
+++ src/main/java/org/apache/maven/plugins/jar/JarMojo.java
@@ -1,5 +1,3 @@
-package org.apache.maven.plugins.jar;
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -9,7 +7,7 @@ package org.apache.maven.plugins.jar;
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
  *
- *  http://www.apache.org/licenses/LICENSE-2.0
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
@@ -18,13 +16,14 @@ package org.apache.maven.plugins.jar;
  * specific language governing permissions and limitations
  * under the License.
  */
+package org.apache.maven.plugins.jar;
+
+import java.io.File;
 
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
-
-import java.io.File;
 
 /**
  * Build a JAR from the current project.
@@ -32,21 +31,23 @@ import java.io.File;
  * @author <a href="evenisse@apache.org">Emmanuel Venisse</a>
  * @version $Id$
  */
-@Mojo( name = "jar", defaultPhase = LifecyclePhase.PACKAGE, requiresProject = true, threadSafe = true,
-       requiresDependencyResolution = ResolutionScope.RUNTIME )
-public class JarMojo
-    extends AbstractJarMojo
-{
+@Mojo(
+        name = "jar",
+        defaultPhase = LifecyclePhase.PACKAGE,
+        requiresProject = true,
+        threadSafe = true,
+        requiresDependencyResolution = ResolutionScope.RUNTIME)
+public class JarMojo extends AbstractJarMojo {
     /**
      * Directory containing the classes and resource files that should be packaged into the JAR.
      */
-    @Parameter( defaultValue = "${project.build.outputDirectory}", required = true )
+    @Parameter(defaultValue = "${project.build.outputDirectory}", required = true)
     private File classesDirectory;
 
     /**
      * Classifier to add to the artifact generated. If given, the artifact will be attached
      * as a supplemental artifact.
-     * If not given this will create the main artifact which is the default behavior. 
+     * If not given this will create the main artifact which is the default behavior.
      * If you try to do that a second time without using a classifier the build will fail.
      */
     @Parameter
@@ -55,24 +56,24 @@ public class JarMojo
     /**
      * {@inheritDoc}
      */
-    protected String getClassifier()
-    {
+    @Override
+    protected String getClassifier() {
         return classifier;
     }
 
     /**
      * {@inheritDoc}
      */
-    protected String getType()
-    {
+    @Override
+    protected String getType() {
         return "jar";
     }
 
     /**
      * {@inheritDoc}
      */
-    protected File getClassesDirectory()
-    {
+    @Override
+    protected File getClassesDirectory() {
         return classesDirectory;
     }
 }
