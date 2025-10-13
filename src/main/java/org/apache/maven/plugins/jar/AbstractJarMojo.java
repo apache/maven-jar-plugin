a4a7d57dcab267aade7320f5662fc45c8a3b37df
Henning P. Schmiedehausen <henning@schmiedehausen.org>
2025-10-13 15:46:34 -0700

    Add new "attach" configuration parameter
    This matches the `attach` parameter from the source plugin
    
    Setting this parameter to `false` will skip attaching the generated jar
    artifact to the build. It will not be picked up by the install and
    deploy goals.
    
    Omitting or setting this parameter to `true` will attach the jar to the
    build. This is the default behavior.
    
    The use case for this is generating test jars as part of a build to be
    used in integration tests while they should not actually be deployed to
    a repository.

--- src/main/java/org/apache/maven/plugins/jar/AbstractJarMojo.java
+++ src/main/java/org/apache/maven/plugins/jar/AbstractJarMojo.java
@@ -173,6 +173,14 @@ public abstract class AbstractJarMojo implements org.apache.maven.api.plugin.Moj
     private Log log;
 
     /**
+     * Specifies whether to attach the jar to the project
+     *
+     * @since 4.0.0-beta-2
+     */
+    @Parameter(property = "maven.jar.attach", defaultValue = "true")
+    protected boolean attach;
+
+    /**
      * Return the specific output directory to serve as the root for the archive.
      * @return get classes directory.
      */
@@ -306,22 +314,27 @@ public abstract class AbstractJarMojo implements org.apache.maven.api.plugin.Moj
         } else {
             Path jarFile = createArchive();
             ProducedArtifact artifact;
-            if (hasClassifier()) {
-                artifact = session.createProducedArtifact(
-                        project.getGroupId(),
-                        project.getArtifactId(),
-                        project.getVersion(),
-                        getClassifier(),
-                        null,
-                        getType());
-            } else {
-                if (projectHasAlreadySetAnArtifact()) {
-                    throw new MojoException("You have to use a classifier "
-                            + "to attach supplemental artifacts to the project instead of replacing them.");
+
+            if (attach) {
+                if (hasClassifier()) {
+                    artifact = session.createProducedArtifact(
+                            project.getGroupId(),
+                            project.getArtifactId(),
+                            project.getVersion(),
+                            getClassifier(),
+                            null,
+                            getType());
+                } else {
+                    if (projectHasAlreadySetAnArtifact()) {
+                        throw new MojoException("You have to use a classifier "
+                                + "to attach supplemental artifacts to the project instead of replacing them.");
+                    }
+                    artifact = project.getMainArtifact().get();
                 }
-                artifact = project.getMainArtifact().get();
+                projectManager.attachArtifact(project, artifact, jarFile);
+            } else {
+                getLog().debug("Skipping attachment of the " + getType() + " artifact to the project.");
             }
-            projectManager.attachArtifact(project, artifact, jarFile);
         }
     }
 
